5cfe6eea2810dbdf00658aa7622b9b21e11d7031
vickygcrb <vicky@ucm.es>
2022-08-04 15:33:56 +0200

    mi primer commit

---
+++ src/proyectoGit2/Main.java
@@ -0,0 +1,16 @@
+package proyectoGit2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduzca un entero");
+		int mov = in.nextInt();
+		System.out.println("usted introdujo: " + mov );
+	}
+
+}
+
