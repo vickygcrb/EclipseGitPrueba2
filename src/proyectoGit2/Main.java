6bdcceb08c6472e8db548bc300b110c4e7401945
vickygcrb <vicky@ucm.es>
2022-08-05 09:21:21 +0200

    Conseguido 2 veces

--- src/proyectoGit2/Main.java
+++ src/proyectoGit2/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("lo he conseguido!!!!!!!!!!!!");
+		System.out.println("lo he conseguido2222222!!!!!!!!!!!!");
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		System.out.println("Introduzca un entero");
